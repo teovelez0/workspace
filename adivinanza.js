7b6d8536c49733c8d8ea7de3c75c7d0924768263
teovelez0 <mateovelez472@gmail.com>
2026-03-17 21:14:20 +0000

    adivinanzas.js

---
+++ adivinanza.js
@@ -0,0 +1,9 @@
+let a =10;
+let b =30;
+if(a==b){
+  console.log("adivinaste");
+}else if(a>b){
+  console.log("muy grande");
+}else if(a<b){
+  console.log("muy pequeño");
+}
---
+++ ciclos.js
@@ -0,0 +1,10 @@
+// Imprime los primeros 50 números pares e impares
+console.log('Primeros 50 pares:');
+for (let i = 1; i <= 50; i++) {
+  console.log(i * 2);
+}
+
+console.log('\nPrimeros 50 impares:');
+for (let i = 0; i < 50; i++) {
+  console.log(i * 2 + 1);
+}
